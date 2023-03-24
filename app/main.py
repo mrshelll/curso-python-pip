bbec729e97e9d427daa14334ec88c165a636cf7c
Miguel Angel D. Parra <>
2023-03-23 22:09:02 -0500

    add Docker

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item: item['Continent'].lower() == 'South America'.lower(), data))
   countries = list(map(lambda item: item['Country/Territory'], data))
   percentages = list(map(lambda item: float(item['World Population Percentage']), data))
   charts.generate_pie_chart(countries, percentages)
-  
+  '''
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'Africa']
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   result = utils.population_by_country(data, country)
 
